c433207a3d83931f0154f19bb782989ea10c5213
fitzMOSO <fitzgerardmoso@gmail.com>
2025-05-16 02:57:33 +0800

    Add service configuration for File Watcher Service
    Updated ProjectInstaller to configure service properties,
    including account type, display name, and start type.
    Removed unnecessary using directives and initialized
    installers to monitor and move files between specified folders.

--- ProjectInstaller.Designer.cs
+++ ProjectInstaller.Designer.cs
@@ -33,12 +33,16 @@
             // 
             // serviceProcessInstaller1
             // 
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
             this.serviceProcessInstaller1.Password = null;
             this.serviceProcessInstaller1.Username = null;
             // 
             // serviceInstaller1
             // 
+            this.serviceInstaller1.Description = "A service that monitors file changes.";
+            this.serviceInstaller1.DisplayName = "File Watcher Service";
             this.serviceInstaller1.ServiceName = "FileWatcherService";
+            this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
             // 
             // ProjectInstaller
             // 
--- ProjectInstaller.cs
+++ ProjectInstaller.cs
@@ -1,19 +1,34 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Configuration.Install;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.ServiceProcess;
 
 namespace FileWatcherService
 {
     [RunInstaller(true)]
     public partial class ProjectInstaller : System.Configuration.Install.Installer
     {
+        private ServiceProcessInstaller processInstaller;
+        private ServiceInstaller serviceInstaller;
+
         public ProjectInstaller()
         {
-            InitializeComponent();
+            // Initialize the service process installer
+            processInstaller = new ServiceProcessInstaller();
+            serviceInstaller = new ServiceInstaller();
+
+            // Configure the service to run under the LocalService account
+            processInstaller.Account = ServiceAccount.LocalService;
+            processInstaller.Username = null;
+            processInstaller.Password = null;
+
+            // Configure the service properties
+            serviceInstaller.ServiceName = "FileWatcherService";
+            serviceInstaller.DisplayName = "File Watcher Service";
+            serviceInstaller.Description = "Monitors C:\\Folder1 for new files and moves them to C:\\Folder2";
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+
+            // Add the installers to the collection
+            Installers.Add(processInstaller);
+            Installers.Add(serviceInstaller);
         }
     }
 }
